38531f56102170918d5f42ee594885ff54e2a264
Tristan Guckenberger <tristanguckenberger@Guys-MacBook-Pro.local>
2018-01-29 10:27:10 -0500

    First commit in final git

---
+++ app/code/InteractOne/LimitStates/Model/ResourceModel/Region/newCollectionAdmin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tristanguckenberger
+ * Date: 1/26/18
+ * Time: 4:52 PM
+ */
+
+namespace InteractOne\LimitStates\Model\ResourceModel\Region;
+
+
+class newCollectionAdmin
+{
+
+}
---
+++ app/code/InteractOne/LimitStates/Model/newCountry.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tristanguckenberger
+ * Date: 1/26/18
+ * Time: 4:20 PM
+ */
+
+namespace InteractOne\LimitStates\Model;
+
+
+class newCountry
+{
+
+}
